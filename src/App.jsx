d92e189d4c479376a2a3cb2c508c7e11a4f84f09
Ahnming Loke <lokeahnming@gmail.com>
2020-01-17 17:07:19 -0500

    feat: add chroma script and helpers.js file to convert colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 
 import Palette from './Palette';
+import { generatePalette } from './helpers';
 import baseColors from './baseColors';
 
 import './App.css';
 
 function App() {
+  console.log(generatePalette(baseColors[4]));
   return (
     <div className="App">
       <Palette {...baseColors[6]}/>
