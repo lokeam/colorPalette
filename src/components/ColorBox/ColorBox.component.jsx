3167bbb5f7b8a258e2fc5507659c21fe867f0445
Ahnming Loke <lokeahnming@gmail.com>
2020-01-16 20:56:03 -0500

    feat: implement copy to clipboard functionality

--- src/components/ColorBox/ColorBox.component.jsx
+++ src/components/ColorBox/ColorBox.component.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.styles.scss';
 
 class ColorBox extends Component {
 
   render() {
+    const { name, background } = this.props;
     return (
-      <div style={{background: this.props.background}} className="colorBox">
-        <span>{ this.props.name }</span>
-        <div>more variants</div>
-      </div>
+      <CopyToClipboard text={ background }>
+        <div style={{background: background }} className="colorBox">
+          <div className="colorBox__copy-container">
+            <div className="colorBox__content">
+              <span>{ name }</span>
+            </div>
+            <button className="colorBox__copy-button">copy</button>
+          </div>
+          <span className="colorBox__see-more">More variants</span>
+        </div>
+      </CopyToClipboard>
     );
   }
 }
