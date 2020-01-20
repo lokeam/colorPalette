691a196e8581b7d2df2f082131ee6cb3fcfe77f6
Ahnming Loke <lokeahnming@gmail.com>
2020-01-20 14:24:48 -0500

    fix: resolve rgba color switching bug with dropdown menu

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,4 @@
 import chroma from "chroma-js";
-
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 function generatePalette(starterPalette) {
@@ -9,38 +8,34 @@ function generatePalette(starterPalette) {
     emoji: starterPalette.emoji,
     colors: {}
   };
-
-  for( let level of levels ) {
+  for (let level of levels) {
     newPalette.colors[level] = [];
   }
-
-  for( let color of starterPalette.colors ) {
+  for (let color of starterPalette.colors) {
     let scale = getScale(color.color, 10).reverse();
-    for ( let i in scale ) {
+    for (let i in scale) {
       newPalette.colors[levels[i]].push({
-        name: `{color.name} ${levels[i]}`,
-        id: color.name.toLowerCase().replace(/ /g, '-'),
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
         hex: scale[i],
-        rgb: chroma(scale[i]).css,
-        rgba: chroma(scale[i]).css().replace('rgb', 'rgba').replace(')','1.0')
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i]).css().replace("rgb", "rgba").replace(")", ",1.0)")
       });
     }
   }
   return newPalette;
 }
-
 function getRange(hexColor) {
-  const lastColor = '#fff';
+  const end = "#fff";
   return [
     chroma(hexColor).darken(1.4).hex(),
     hexColor,
-    lastColor
+    end
   ];
 }
 
-function getScale(hexColor, numberOfColorsToGenerate) {
-  return chroma
-    .scale(getRange(hexColor)).mode('lab').colors(numberOfColorsToGenerate);
+function getScale(hexColor, numberOfColors) {
+  return chroma.scale(getRange(hexColor)).mode("lab").colors(numberOfColors);
 }
 
-export {generatePalette};
+export { generatePalette };
