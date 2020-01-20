980950ea27f72dde3339c79e9e9c931c6089becf
Ahnming Loke <lokeahnming@gmail.com>
2020-01-20 14:49:42 -0500

    feat: add material-ui/icons, added poc bottom left notification element that alerts upon format change

--- src/components/Nav/Navigation.component.jsx
+++ src/components/Nav/Navigation.component.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navigation.styles.scss';
@@ -9,13 +13,24 @@ class Navigation extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {format: "hex"};
+    this.state = {
+      format: "hex",
+      open: false
+    };
     this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   };
+
+  closeSnackbar() {
+    this.setState({
+      open: false
+    })
+  }
 
   handleFormatChange(e) {
     this.setState({
-      format: e.target.value
+      format: e.target.value,
+      open: true
     });
     this.props.handleChange(e.target.value);
   }
@@ -46,6 +61,23 @@ class Navigation extends Component {
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar anchorOrigin={{vertical: "bottom", horizontal:"left"}}
+                  open={this.state.open}
+                  autoHideDuration={3000}
+                  message={<span id="message-id">Format Changed to {format.toUpperCase()} </span>}
+                  ContentProps={{
+                    "aria-describedBy": "message-id"
+                  }}
+                  onClose={this.closeSnackbar}
+                  action={[
+                    <IconButton onClick={this.closeSnackbar}
+                                color="inherit"
+                                key="close"
+                                aria-label="close">
+                      <CloseIcon />
+                    </IconButton>
+                  ]}
+        />
       </nav>
     );
   }
