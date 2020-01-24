2ff3cc039d98f8b067fab5d299adf7e400dcc66a
Ahnming Loke <lokeahnming@gmail.com>
2020-01-23 22:27:18 -0500

    style: added mini color boxes to each palette

--- src/pages/MiniPalette/MiniPalette.component.jsx
+++ src/pages/MiniPalette/MiniPalette.component.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import './MiniPalette.styles.scss';
 
 function MiniPalette(props) {
-  const { paletteName, emoji } = props;
+  const { paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div className="minipalette__minicolorbox"
+         style={{backgroundColor: color.color}}
+         key={color.name}
+    ></div>
+  ));
+
   return (
     <div className="minipalette">
-      <div className="minipalette__colors"></div>
+      <div className="minipalette__colors">
+        { miniColorBoxes }
+      </div>
       <h5 className="minipalette__title">{ paletteName }
         <span className="minipalette__emoji">{emoji}</span>
       </h5>
--- src/pages/Palette/PaletteList.jsx
+++ src/pages/Palette/PaletteList.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import MiniPalette from '../MiniPalette/MiniPalette.component';
 import { Link } from 'react-router-dom';
+import './PaletteList.styles.scss';
 
 class PaletteList extends Component {
 
   render() {
     const { palettes } = this.props;
     return (
-      <div>
-        <MiniPalette />
-        <h1>React Colors</h1>
-        { palettes.map(palette => (
-          <MiniPalette {...palette}/>
-        )) }
+      <div className="palettelist">
+        <div className="palettelist__container">
+          <div className="palettelist__title">
+            <h1>color palette</h1>
+          </div>
+          <div className="palettelist__minipalette-container">
+            { palettes.map(palette => (
+              <MiniPalette {...palette}/>
+            )) }
+        </div>
+        </div>
       </div>
     );
   };
